00ffec076f19c347c13936e4c47ee7475456725b
Peter <peter.debeer@dare.it>
2018-03-02 11:50:46 +0100

    Create basic setup for notification manger

---
+++ src/NotificationManager.php
@@ -0,0 +1,37 @@
+<?php
+declare(strict_types=1);
+
+namespace NotificationManager;
+
+use NotificationManager\Map\NotificationHandlerMapInterface;
+use NotificationManager\Notifications\NotificationInterface;
+use Psr\Log\LoggerInterface;
+
+final class NotificationManager
+{
+    /** @var NotificationHandlerMapInterface */
+    private $notificationHandlerMap;
+
+    /** @var LoggerInterface */
+    private $logger;
+
+    /**
+     * NotificationManager constructor.
+     * @param NotificationHandlerMapInterface $notificationHandlerMap
+     * @param LoggerInterface                 $logger
+     */
+    public function __construct(NotificationHandlerMapInterface $notificationHandlerMap, LoggerInterface $logger)
+    {
+        $this->notificationHandlerMap = $notificationHandlerMap;
+        $this->logger = $logger;
+    }
+
+    /**
+     * @param NotificationInterface $notification
+     */
+    public function dispatch(NotificationInterface $notification): void
+    {
+        $handler = $this->notificationHandlerMap->getHandler($notification);
+        $handler->handle($notification);
+    }
+}
