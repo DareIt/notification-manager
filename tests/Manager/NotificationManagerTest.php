4b99fd2d645be37512a6ce2999ca29274133b2c1
Peter <peter.debeer@dare.it>
2018-03-02 11:52:27 +0100

    Add test for basic use of the notification manger

---
+++ tests/Manager/NotificationManagerTest.php
@@ -0,0 +1,40 @@
+<?php
+declare(strict_types=1);
+
+namespace NotificationManager\Tests\Manager;
+
+use NotificationManager\Map\InMemoryNotificationHandlerMap;
+use NotificationManager\NotificationManager;
+use NotificationManager\Notifications\NotificationInterface;
+use NotificationManager\Tests\TestHelpers\HandlerTestHelper;
+use NotificationManager\Tests\TestHelpers\LoggerTestHelper;
+use NotificationManager\Tests\TestHelpers\NotificationTestHelper;
+use NotificationManager\Tests\TestHelpers\WriterTestHelper;
+use PHPUnit\Framework\TestCase;
+
+final class NotificationManagerTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function canHandelNotificationEnWriteThem()
+    {
+        $handler = HandlerTestHelper::get($this);
+        $testData = ['foo' => 'foo', 'bar' => 'bar', 'fooBar' => 'fooBar'];
+        $notification = NotificationTestHelper::get($this, $testData);
+
+        $notificationHandlerMap = new InMemoryNotificationHandlerMap();
+        $notificationHandlerMap->mapNotificationAndHandler($notification, $handler);
+
+        $testLogger = LoggerTestHelper::get();
+
+        $notificationManager = new NotificationManager($notificationHandlerMap, $testLogger);
+        $notificationManager->dispatch($notification);
+
+        $this->assertCount(1, WriterTestHelper::$notifications);
+        $notifications = WriterTestHelper::$notifications;
+        $notification = array_shift($notifications);
+        $this->assertInstanceOf(NotificationInterface::class, $notification);
+    }
+
+}
